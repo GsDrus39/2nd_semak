e1bbd3331238346baf043004a6cde48d3ee67829
GsDrus39 <63257358+GsDrus39@users.noreply.github.com>
2023-06-22 20:46:48 +0200

    Update 4.cpp

--- 4pract/4.cpp
+++ 4pract/4.cpp
@@ -13,25 +13,41 @@ public:
     void poll() {
         std::cout << name << std::endl;
     }
+    
+    std::string get_name(){
+        return name;
+    }
 };
 
 class Counter : public Device {
 public:
     explicit Counter(const std::string &name) : Device(name) {};
+    void poll() {
+        std::cout << get_name() << std::endl;
+        /* do something */
+    };
 };
 
 class DiscreteSignal : public Device {
 public:
     explicit DiscreteSignal(const std::string &name) : Device(name) {};
+    void poll() {
+        std::cout << get_name() << std::endl;
+        /* do something */
+    };
 };
 
 class Heating : public Device {
 public:
     explicit Heating(const std::string &name) : Device(name) {};
+    void poll() {
+        std::cout << get_name() << std::endl;
+        /* do something */
+    };
 };
 
 std::string counters[] = {
-        "Меркурий 230", "Меркурий 230", "Энергомера CE308"
+        "Меркурий 230", "Нева МТ314", "Энергомера CE308"
 };
 std::string discreteSignals[] = {
         "Reallab NL-16HV", "Приборэлектро PRE-16", "Энергосервис ЭНМВ-1-24"
@@ -50,13 +66,13 @@ int main() {
     setlocale(LC_ALL, "ru");
     std::ifstream devices("../config.txt");
     std::string currentDevice;
-
     while (getline(devices, currentDevice)) {
         int n = sizeof(counters) / sizeof(std::string);
         for (int i = 0; i < n; i++) {
             if (counters[i] == currentDevice) {
                 Counter *counter = new Counter(currentDevice);
                 configuration.counter = counter;
+                configuration.counter->poll();
             }
         }
         n = sizeof(discreteSignals) / sizeof(std::string);
@@ -64,6 +80,7 @@ int main() {
             if (discreteSignals[i] == currentDevice) {
                 DiscreteSignal *discreteSignal = new DiscreteSignal(currentDevice);
                 configuration.discreteSignal = discreteSignal;
+                configuration.discreteSignal->poll();
             }
         }
         n = sizeof(heatings) / sizeof(std::string);
@@ -71,6 +88,7 @@ int main() {
             if (heatings[i] == currentDevice) {
                 Heating *heating = new Heating(currentDevice);
                 configuration.heating = heating;
+                configuration.heating->poll();
             }
         }
     }
